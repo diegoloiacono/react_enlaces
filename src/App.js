4f05c2f8e782fb2cfe17f4b217ade378fa47b55e
Diego Lo Iacono <da.loiacono@gmail.com>
2022-07-04 14:11:10 +0200

    added errormessage, fixed registerform

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<EntriesPage />} />
-          <Route path="*" element={<NotFoundPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </main>
       <Footer />
---
+++ src/components/ErrorMessage/index.js
@@ -0,0 +1,5 @@
+const ErrorMessage = ({ error }) => {
+  return <p className="error_message">{error}</p>;
+};
+
+export default ErrorMessage;
