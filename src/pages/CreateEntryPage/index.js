1280f448061c418cc641bbdece992434acbe44d1
Diego Lo Iacono <da.loiacono@gmail.com>
2022-07-05 20:07:49 +0200

    added createEntry

---
+++ src/pages/CreateEntryPage/index.js
@@ -0,0 +1,13 @@
+import CreateEntryForm from "../../components/CreateEntryForm";
+
+const CreateEntryPage = () => {
+  return (
+    <section>
+      <h2>Upload URL</h2>
+
+      <CreateEntryForm />
+    </section>
+  );
+};
+
+export default CreateEntryPage;
